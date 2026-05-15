d530d7009a659776d12a809f5d5bd66fa0eac6f1
MarsMay <xiaohua.mei@kunlun-inc.com>
2026-05-15 09:13:39 +0800

    fix: convert Formula to Cask DSL format

--- Casks/usageboard.rb
+++ Casks/usageboard.rb
@@ -1,18 +1,18 @@
-class Usageboard < Formula
-  desc "macOS menu bar app for API usage tracking"
-  homepage "https://github.com/marsmay/UsageBoard"
+cask "usageboard" do
   version "0.1.23"
   sha256 "3413b8d8ed0f06db915284e4aaabaedfca9fbfaca2f857f17b849084f7083f5c"
 
   url "https://may.ltd/usageboard/UsageBoard-#{version}.zip"
+  name "UsageBoard"
+  desc "macOS menu bar app for API usage tracking"
+  homepage "https://github.com/marsmay/UsageBoard"
 
   depends_on macos: ">= :ventura"
 
   app "UsageBoard.app"
 
-  def caveats
-    <<~EOS
-      UsageBoard will appear in your menu bar after launch.
-    EOS
-  end
+  zap trash: [
+    "~/Library/Application Support/UsageBoard",
+    "~/Library/Caches/UsageBoard",
+  ]
 end

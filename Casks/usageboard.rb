2854b06b9fce74fe705cebb1f039e698511be33f
Mars May <i@may.ltd>
2026-07-19 00:54:15 +0800

    fix: 补充 UsageBoard 下载域名验证

--- Casks/usageboard.rb
+++ Casks/usageboard.rb
@@ -2,7 +2,8 @@ cask "usageboard" do
   version "0.1.35"
   sha256 "0df7dd28df934363d0dcc00d020b8a27f9108ef39b44ba2b0ebfc51d956cb4f1"
 
-  url "https://may.ltd/usageboard/UsageBoard-#{version}.zip"
+  url "https://may.ltd/usageboard/UsageBoard-#{version}.zip",
+      verified: "may.ltd/usageboard/"
   name "UsageBoard"
   desc "macOS menu bar app for API usage tracking"
   homepage "https://github.com/marsmay/UsageBoard"

9318bd0fa31d7e59a41b1bff4b0d1e764eccb10d
Mars May <i@may.ltd>
2026-08-12 11:38:31 +0800

    Update UsageBoard cask to 0.1.37

--- Casks/usageboard.rb
+++ Casks/usageboard.rb
@@ -1,6 +1,6 @@
 cask "usageboard" do
-  version "0.1.36"
-  sha256 "6d7f3b5ab5fd93fcfccd65d67ad225ee761e25bbde57b11eba51a2594bdaf269"
+  version "0.1.37"
+  sha256 "c3fe46ff686d688a5515e679bc920305da161c9c4293da0b3e4de88d6fb1b7fa"
 
   url "https://may.ltd/usageboard/UsageBoard-#{version}.zip",
       verified: "may.ltd/usageboard/"

1a7367adf9e09cd8f001e6b3a4a480119e22040a
Mars May <i@may.ltd>
2026-09-11 00:16:47 +0800

    usageboard 1.0.2

--- Casks/usageboard.rb
+++ Casks/usageboard.rb
@@ -1,6 +1,6 @@
 cask "usageboard" do
-  version "1.0.1"
-  sha256 "1d4c935820a657b98a7bcf4c793745c3a66237a3fc6affdaf6c820692e1c0f42"
+  version "1.0.2"
+  sha256 "600f51755a8728ca73ec4f2362ec8b919136870a349a554ca2668baecc7d179a"
 
   url "https://may.ltd/usageboard/UsageBoard-#{version}.zip",
       verified: "may.ltd/usageboard/"

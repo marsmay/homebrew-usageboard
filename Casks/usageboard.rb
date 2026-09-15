a3301cb3163d55d2980030d8139027501a4124cf
Mars May <i@may.ltd>
2026-09-15 19:43:38 +0800

    fix(cask): remove deprecated URL verified parameter

--- Casks/usageboard.rb
+++ Casks/usageboard.rb
@@ -2,8 +2,7 @@ cask "usageboard" do
   version "1.0.5"
   sha256 "b6eaaffa21e7afad58124d01890a824b18b9e1cba3547b7dd2d8c72bd08ed16c"
 
-  url "https://may.ltd/usageboard/UsageBoard-#{version}.zip",
-      verified: "may.ltd/usageboard/"
+  url "https://may.ltd/usageboard/UsageBoard-#{version}.zip"
   name "UsageBoard"
   desc "Menu bar app for API usage tracking"
   homepage "https://github.com/marsmay/UsageBoard"
